17390d19e1ac271ba0f59ecda0192115c2e3a275
lidiyag9 <lidiyagoryacheva@mail.ru>
2016-12-22 19:28:03 +0300

    Add files via upload

--- Jscript1.js
+++ Jscript1.js
@@ -38,41 +38,44 @@ function check() {
     rese.innerHTML= "Вы уверены, что " + answer + '?';
 
 }
-
-function check_me()
-{
-    var count=0;
-    with(document.test) {
-if (!Q1[0].checked&&!Q1[1].checked&&!Q1[2].checked)  
-{count+=1};  
-if (!Q2[0].checked&&!Q2[1].checked&&!Q2[2].checked)  
-{count+=1};  
-if (!Q3[0].checked&&!Q3[1].checked&&!Q3[2].checked)  
-{ count += 1 };
-if (!Q4[0].checked && !Q4[1].checked && !Q4[2].checked)
-{ count += 1 };
-if (count>0) alert("Вы выполнили не все задания. Проверьте себя!")    
+var res = "11111";
+function check_me() {
+    var count = 0;
+    with (document.test) {
+        if (!Q1[0].checked && !Q1[1].checked && !Q1[2].checked && !Q1[3].checked)
+        { count += 1 };
+        if (!Q2[0].checked && !Q2[1].checked && !Q2[2].checked && !Q2[3].checked)
+        { count += 1 };
+        if (!Q3[0].checked && !Q3[1].checked && !Q3[2].checked && !Q3[3].checked)
+        { count += 1 };
+        if (!Q4[0].checked && !Q4[1].checked && !Q4[2].checked && !Q4[3].checked)
+        { count += 1 };
+        if (!Q5[0].checked && !Q5[1].checked && !Q5[2].checked && !Q5[3].checked)
+        { count += 1 };
+        if (count > 0) alert("Вы выполнили не все задания. Проверьте себя!")
         else answer();
     }
-} 
+}
  
-function control(k, f1,f2,f3,f4) {
-if (k==1&&f1.checked) return true;
-if (k==2&&f2.checked) return true;
-if (k == 3 && f3.checked) return true;
-if (k == 4 && f4.checked) return true;
-return false;
+function control(k, f1, f2, f3, f4, f5) {
+    if (k == 1 && f1.checked) return true;
+    if (k == 2 && f2.checked) return true;
+    if (k == 3 && f3.checked) return true;
+    if (k == 4 && f4.checked) return true;
+    if (k == 5 && f5.checked) return true;
+    return false;
 }
 
 function answer() {
-answ="";
-     with(document)    {
-    answ+=control(res.charAt(0) ,test.Q1[0],test.Q1[1],test.Q1[2])?"1":"0";
-answ+=control(res.charAt(1) ,test.Q2[0],test.Q2[1],test.Q2[2])?"1":"0";
-answ+=control(res.charAt(2) ,test.Q3[0],test.Q3[1],test.Q3[2])?"1":"0";
-answ += control(res.charAt(3), test.Q4[0], test.Q4[1], test.Q4[2]) ? "1" : "0";
+    answ = "";
+    with (document) {
+        answ += control(res.charAt(0), test.Q1[0], test.Q1[1], test.Q1[2], test.Q1[3]) ? "1" : "0";
+        answ += control(res.charAt(1), test.Q2[0], test.Q2[1], test.Q2[2], test.Q2[3]) ? "1" : "0";
+        answ += control(res.charAt(2), test.Q3[0], test.Q3[1], test.Q3[2], test.Q3[3]) ? "1" : "0";
+        answ += control(res.charAt(3), test.Q4[0], test.Q4[1], test.Q4[2], test.Q4[3]) ? "1" : "0";
+        answ += control(res.charAt(4), test.Q5[0], test.Q5[1], test.Q5[2], test.Q5[3]) ? "1" : "0";
 
-showResult();
+        showResult();
     }
 }
  
@@ -81,20 +84,10 @@ function showResult()   {
     var i,s;
  
 for (i=0; i<answ.length;i++) {nok+=answ.charAt(i)=="1"?1:0;}
-if(nok==4) s="ОТЛИЧНО";
-if(nok<4) s="ХОРОШО";
-if(nok<3.25) s="УДОВЛЕТВОРИТЕЛЬНО";
-if (nok<2.5) s="НЕУДОВЛЕТВОРИТЕЛЬНО";
-    document.test.s1.value="Количество правильных ответов "+nok+". Ваша оценка "+s+".";
+
+    document.test.s1.value="Количество правильных ответов "+nok+".";
  
-with(document.test)
-    {
-    if (answ.charAt(0)=="1") {T1.value=" + "} else {T1.value=" - "};
-   if (answ.charAt(1)=="1") {T2.value=" + "} else {T2.value=" - "};
-   if (answ.charAt(2) == "1") { T3.value = " + " } else { T3.value = " - " };
-   if (answ.charAt(3) == "1") { T4.value = " + " } else { T4.value = " - " };
      }
-}
 function showhide(obj){
     if(obj == 'none') return 'inline';
     else return 'none';
